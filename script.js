9cd75d7ff15086b835f0c1d35751040af55b473c
Genesis <genesisybenitez1@gmail.com>
2021-10-26 23:24:50 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+var two = 2;
+var oneFourth = "1/4";
+function marinaraSauce(cookName) {
+    console.log("Hey, " + cookName + "! This is my marinara sauce recipe!");
+    console.log("- Heat a medium-large saucepan over medium heat.");
+    console.log();
+    console.log("- Add " + two + " tspns of oil and 5 carlic cloves, cook until golden, about " + two + " minutes");
+    console.log();
+    console.log("- Add " + oneFourth + " cup water, " + two + " cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log();
+    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log();
+    console.log("- Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+
+marinaraSauce("Genesis")
+
+var numberArray = [1,33,4,223,928];
+var stringArray = ["Gen", "atl", "nyc"];
+var objectArray = [{name: "gen", age: 27}, {name: "Al", age: 26}];
+// console.log(numberArray[0]);
+// console.log(stringArray[0]);
+// console.log(stringArray[1]);
+// console.log("number array has " + numberArray.length + " items");
+// console.log("string array has " + stringArray.length + " items");
+// console.log("object array has " +objectArray.length + " items");
+
+// console.log(stringArray.toString());
+// console.log(numberArray.toString());
+
+// console.log(stringArray.join("*"));
+
+// stringArray.pop()
+// stringArray.push("hello");
+// stringArray[0] = "dc";
+// console.log(numberArray.toString());
+
+// var genAnimal ={
+// species: "wolf",
+// origin: "europe",
+// age: 6,
+// isNuetered: false
+// }
+// console.log(genAnimal.species);
+// console.log(Object.keys(genAnimal).toString());
+
+// for(let i = 0; i < numberArray.length; i++){
+//     numberArray[i] = i;
+// }
+// console.log(numberArray);
+
+for(let i =5; i > 0; i--){
+    console.log(i);
+}
+// i=0
+// while(i < 10){
+// console.log(i);
+// i++;
+// }
